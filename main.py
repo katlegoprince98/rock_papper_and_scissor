e6585df27cd9b9471d8c841c7942ace66e19000e
katlegoprince98 <CynicsTech1998@gmail.com>
2023-03-10 17:14:56 +0200

    final

--- main.py
+++ main.py
@@ -29,28 +29,35 @@ scissors = '''
 
 comp = random.randint(1,3)
 user = int(input("What do you choose, 1-for rock, 2-papper and 3-scissors"))
+game_imga = [rock, paper, scissors]
 
-if user == 1 and comp == 3:
+if user > 2 or user < 0:
+   print("Invalid choice")
+else:
+   print(game_imga[user])
+   
+
+if user == 0 and comp == 2:
   print(f"You chose {user} and the computer chose {comp}")
   print("You win")
   print(rock)
-elif user == 3 and comp == 1:
+elif user == 2 and comp == 0:
   print(f"You chose {user} and the computer chose {comp}")
   print("You lose")
   print(rock)
-elif user == 3 and comp == 2:
-   print(f"You chose {user} and the computer chose {comp}")
-   print("You win")
-   print(scissors)
-elif user == 2 and comp == 3:
-   print(f"You chose {user} and the computer chose {comp}")
-   print("You lose")
-   print(scissors)
 elif user == 2 and comp == 1:
    print(f"You chose {user} and the computer chose {comp}")
    print("You win")
-   print(paper)
+   print(scissors)
 elif user == 1 and comp == 2:
+   print(f"You chose {user} and the computer chose {comp}")
+   print("You lose")
+   print(scissors)
+elif user == 1 and comp == 0:
+   print(f"You chose {user} and the computer chose {comp}")
+   print("You win")
+   print(paper)
+elif user == 0 and comp == 1:
    print(f"You chose {user} and the computer chose {comp}")
    print("You win")
    print(paper)
